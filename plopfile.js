1cdd9208120c3ef64ba6bc734086a92b2946cc16
Amo_Taiki_from_Desktop <c1101081@planet.kanazawa-it.ac.jp>
2024-02-04 01:11:19 +0900

    Update choices in README prompt

--- plopfile.js
+++ plopfile.js
@@ -8,7 +8,7 @@ module.exports = (
                 type: "checkbox",
                 name: "MultipleChoice",
                 message: "What do you want to include in your README?",
-                choices: ["React","Next.js","Vue.js","Nuxt.js","Angular","Svelte","Express.js","Solid.js","Playwright","typescript","javascript","storybook","tailwindcss","daisyui"]
+                choices: ["React","Next.js","Vue.js","Nuxt.js","Angular","Svelte","Express.js","Solid.js","Playwright","typescript","javascript","storybook","TailwindCSS","DaisyUI"]
         }],
         actions: (
             data
